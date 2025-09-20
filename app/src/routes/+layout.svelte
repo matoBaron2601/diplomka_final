2ee03de2f396037d656209086c1554cab38a37f8
Martin Baron <mato.baron12@gmail.com>
2025-09-20 14:40:09 +0200

    main commit

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import AppSidebar from '$lib/components/AppSidebar.svelte';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Sidebar.Provider>
+	<AppSidebar />
+	<main>
+		<Sidebar.Trigger />
+		{@render children?.()}
+	</main>
+</Sidebar.Provider>
---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	const fetchData = async () => {
+		const response = await fetch('/api');
+		const data = await response.json();
+		return data;
+	};
+</script>
+
+<div>
+	<button on:click={fetchData}>Fetch Data</button>
+</div>
